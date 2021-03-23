6e75c9bea575a1e72a418363d61f5178277d348b
pinkchocoa <pinkchocoa@gmail.com>
2021-03-23 14:42:12 +0800

    from speakText import speakText

--- speakText.py
+++ speakText.py
@@ -4,19 +4,22 @@
 #from num2words import num2words #idk why this is needed
 from subprocess import call
 
-cmdBegin = 'espeak '
-cmdEnd='" 2>/dev/null'
-cmdOut='"'
+def speakText(input):
+	cmdBegin = 'espeak '
+	cmdEnd='" 2>/dev/null'
+	cmdOut='"'
+	cmdOut+=text
+	cmdFinal=cmdBegin+cmdOut+cmdEnd
+	call([cmdFinal], shell=True)
+	return cmdFinal #to check command
+
 #cmdEnd = ' | aplay /home/pi/Destop/text.wav 2>/dev/null' # play text.wav and dump errors to /dev/null
 #cmdOut = '--stdout > /home/pi/Desktop/text.wav ' #store the voice file
 
+#testing
 text = input("Enter text: ")
 print("speaking... ", text)
 #text = text.replace(' ', '_') #need to replace spaces with underscores to identify as a command
-cmdOut += text
+#cmdOut += text
+print(speakText(text))
 
-cmdFinal = cmdBegin+cmdOut+cmdEnd
-print("running cmd: ", cmdFinal)
-
-
-call([cmdFinal], shell=True)

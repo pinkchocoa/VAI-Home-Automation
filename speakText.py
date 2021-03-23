90a9eb7ec5c992bb7de7d0c14f64edec5e30c1f1
pinkchocoa <pinkchocoa@gmail.com>
2021-03-23 14:25:40 +0800

    it says stuff now by running cmd in cmdline, see speakText.py

--- speakText.py
+++ speakText.py
@@ -1,1 +1,22 @@
+#!/usr/bin/env python3
 
+#pip install num2words
+#from num2words import num2words #idk why this is needed
+from subprocess import call
+
+cmdBegin = 'espeak '
+cmdEnd='" 2>/dev/null'
+cmdOut='"'
+#cmdEnd = ' | aplay /home/pi/Destop/text.wav 2>/dev/null' # play text.wav and dump errors to /dev/null
+#cmdOut = '--stdout > /home/pi/Desktop/text.wav ' #store the voice file
+
+text = input("Enter text: ")
+print("speaking... ", text)
+#text = text.replace(' ', '_') #need to replace spaces with underscores to identify as a command
+cmdOut += text
+
+cmdFinal = cmdBegin+cmdOut+cmdEnd
+print("running cmd: ", cmdFinal)
+
+
+call([cmdFinal], shell=True)

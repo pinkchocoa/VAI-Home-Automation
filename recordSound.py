f8bdc4bac5d5fe85a4bf04739c62a2568fca388c
pinkchocoa <pinkchocoa@gmail.com>
2021-03-27 03:47:53 +0800

    put them in functions..

--- recordSound.py
+++ recordSound.py
@@ -3,16 +3,28 @@ import os
 import time
 import signal
 
-outputFile = 'out'
-record = 'arecord --format=S16_LE --rate=16000 --file-type=wav '
-record += outputFile
-record += '.wav'
 
-p = subprocess.Popen(record, shell=True, preexec_fn=os.setsid)
-print("startRecordingArecord()> p pid= " + str(p.pid))
-print("startRecordingArecord()> recording started")
-time.sleep(20)
-os.killpg(p.pid, signal.SIGTERM)
-p.terminate()
-p = None
-print("stopRecordingArecord()> Recording stopped")
+def recordSound(output, duration):
+	record = 'arecord --format=S16_LE --rate=16000 --file-type=wav '
+	record += output
+	record += '.wav'
+
+	p = subprocess.Popen(record, shell=True, preexec_fn=os.setsid)
+	print("startRecordingArecord()> p pid= " + str(p.pid))
+	print("startRecordingArecord()> recording started")
+	time.sleep(duration)
+	os.killpg(p.pid, signal.SIGTERM)
+	p.terminate()
+	p = None
+	print("stopRecordingArecord()> Recording stopped")
+
+def playSound(input):
+	arg = 'aplay '
+	arg += input
+	arg += '.wav'
+	print("playing: " + arg)
+	subprocess.call(arg, shell=True)
+
+
+recordSound('out', 5)
+playSound('out')

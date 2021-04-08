eed7d3cf321b864dc9b6954252d8a993d19cb2e3
jzxr <jasminezxr@gmail.com>
2021-04-08 21:28:21 +0800

    make sure its updated

--- LFPub1.py
+++ LFPub1.py
@@ -9,51 +9,6 @@ from audio.speakText import speakText
 from fileio import file_to_set, write_file, append_to_file, delete_file_contents
 
 GPIO.setmode(GPIO.BCM)
-#     
-# #Temperature Sensor TMP102
-# i2c_ch = 1
-# i2c_address = 0x48
-# reg_temp = 0x00
-# reg_config = 0x01
-# 
-# def twos_comp(val, bits):
-#     if (val & (1 << (bits-1))) != 0:
-#         val = val-(1<<bits)
-#     return val
-# 
-# def read_temp():
-#     val = bus.read_i2c_block_data(i2c_address, reg_temp, 2)
-#     temp_c = (val[0] << 4) | (val[1] >> 5)
-# 
-#     temp_c = twos_comp(temp_c, 12)
-# 
-#     temp_c = temp_c * 0.0625
-# 
-#     return temp_c
-# 
-# bus = smbus.SMBus(i2c_ch)
-# 
-# val = bus.read_i2c_block_data(i2c_address, reg_config, 2)
-# print ("Old CONFIG:", val)
-# 
-# val[1] = val[1] & 0b00111111
-# val[1] = val[1] | (0b10 << 6)
-# 
-# bus.write_i2c_block_data(i2c_address, reg_config,val)
-# 
-# val = bus.read_i2c_block_data(i2c_address, reg_config, 2)
-# print("New CONFIG:", val)
-# 
-# #Light Dependent Resistor LDR sensor
-# def RCtime (RCpin):
-#     reading = 0
-#     GPIO.setup(RCpin, GPIO.OUT)
-#     GPIO.output(RCpin, GPIO.LOW)
-#     time.sleep(0.5)
-#     GPIO.setup(RCpin, GPIO.IN)
-#     while (GPIO.input(RCpin) == GPIO.LOW):
-#            reading += 1
-#     return reading
 
 def checkInput(said, inputs):
     if said is None or not said:
@@ -106,25 +61,11 @@ mqttc = mqtt.Client('python_pub')
 mqttc.connect(MQTTBROKER, PORT)
 mqttc.publish(TOPIC, MESSAGE)
 
-LightButtonPress = True
-FanButtonPress = True
-ledState = False
-fanState = False
-
-
 while True:
-#     #Light
-#     print (RCtime(17))
-#     #Fan
-#     temperature = read_temp()
-#     print(round(temperature, 1), "C")
-#     time.sleep(2)
-    ledState = False
-    fanState = True
+
     said = micRec(3)
     #check textfile
     textfile = 'status.txt'
-    #if textfile is not empty
     content = file_to_set (textfile)
     print(content)
     for i in content:
@@ -135,10 +76,6 @@ while True:
             break
         print(content)
 
-            
-            
-    
-    #e.g. lightWeb = whatever is in the txtfile
     if checkInput(said, ['light', 'on']) or checkInput(content, ['LED', 'On']):
         #reset variable
         white()

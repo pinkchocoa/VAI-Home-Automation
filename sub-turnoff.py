1e0b437831ec92dfa1dd46cc506407763f367bad
jzxr <jasminezxr@gmail.com>
2021-08-31 15:45:13 +0800

    create turnoff python to turn off everything (lights and fans)

---
+++ sub-turnoff.py
@@ -0,0 +1,33 @@
+import paho.mqtt.client as mqtt
+import RPi.GPIO as GPIO, time, os
+import time
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setwarnings(False)
+
+#set fan pin 
+FAN_PIN1 = 20
+
+#set red,green and blue pins
+redPin = 5
+greenPin = 6
+bluePin = 13
+
+#set pins as outputs
+GPIO.setup(redPin,GPIO.OUT)
+GPIO.setup(greenPin,GPIO.OUT)
+GPIO.setup(bluePin,GPIO.OUT)
+
+def fanOff():
+    #set as fan off
+    GPIO.setup(FAN_PIN1, True)
+
+def lightOff():
+    GPIO.output(redPin,GPIO.HIGH)
+    GPIO.output(greenPin,GPIO.HIGH)
+    GPIO.output(bluePin,GPIO.HIGH)
+    
+fanOff()
+lightOff()
